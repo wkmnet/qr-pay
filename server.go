a047ad1edbb8e51500824d94b7d5b8a237724a51
wukm <wukm@wukm-server.vmovier.cc>
2018-04-10 17:29:04 +0800

    init project

---
+++ server.go
@@ -0,0 +1,18 @@
+/**
+ * Create with IntelliJ IDEA
+ * Project name : qr-pay
+ * Package name : 
+ * Author : Wukunmeng
+ * User : wukm
+ * Date : 18-4-10
+ * Time : 下午5:23
+ * ---------------------------------
+ * 
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello")
+}
